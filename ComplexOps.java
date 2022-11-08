61c5c24cd66f18d6f7df6a4bfad4c0650098c7a5
Mr3lueSky <78630867+Mr3lueSky@users.noreply.github.com>
2022-11-08 09:52:55 +0530

    Code Completed

--- ComplexOps.java
+++ ComplexOps.java
@@ -1,12 +1,26 @@
-import java.util.*;
 class Complex{
 int real1,real2,img1,img2;
+
 void sum(){
-int realsum = real1+real2;
-int imgsum = img1+img2;
-System.out.println("Sum = "+realsum+"+i"+imgsum);
+	int realsum = real1+real2;
+	int imgsum = img1+img2;
+	System.out.println("Sum = "+realsum+"+i"+imgsum);
+	}
+	
+void difference(){
+	int realdiff = real1-real2;
+	int imgdiff = img1-img2;
+	System.out.println("Difference ="+realdiff+"+i"+imgdiff);
+	}
+
+void product(){
+	int realprod = real1*real2;
+	int imgprod = img1*img2;
+	System.out.println("Product = "+realprod+"+i"+imgprod);
+	}
 }
-}
+
+
 public class ComplexOps{
 public static void main(String[] args){
 Scanner s = new Scanner(System.in);
@@ -20,5 +34,7 @@ num1.img1=s.nextInt();
 System.out.println("Enter img2: ");
 num1.img2=s.nextInt();
 num1.sum();
+num1.difference();
+num1.product();
 }
 }
